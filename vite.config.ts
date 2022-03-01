34e269c903a96e83fc03fb4ff905e16f9af87213
adekang <adekang@163.com>
2022-03-01 18:47:13 +0800

    fix: 添加移动端调试

--- vite.config.ts
+++ vite.config.ts
@@ -20,7 +20,7 @@ export default defineConfig(configEnv => {
     plugins: createVitePlugins(),
     css: cssOption,
     server: {
-      host: false,
+      host: true,
       port: VITE_APP_PORT,
       open: VITE_APP_OPEN,
       proxy
